dc4f312ba934a6581d44d9658adc645600165744
1140725847@qq.com <test>
2019-10-12 10:36:25 +0800

    1

--- bendiyou/src/App.vue
+++ bendiyou/src/App.vue
@@ -2,7 +2,7 @@
  * @Description: In User Settings Edit
  * @Author: your name
  * @Date: 2019-10-11 15:31:13
- * @LastEditTime: 2019-10-12 10:24:52
+ * @LastEditTime: 2019-10-12 10:34:39
  * @LastEditors: Please set LastEditors
  -->
 <template>
@@ -21,22 +21,17 @@
           <footer>
             <van-tabbar
               style=" background-color: #fff!important;
-    box-shadow: 0 0 10px 0 hsla(0, 6%, 58%, 0.6) !important;"
+    box-shadow: 0 0 10px 0 hsla(0, 6%, 58%, 0.6) !important;height: 50px"
               v-model="active"
               active-color="#EB111F"
               inactive-color="#000"
               route
             >
-              <van-tabbar-item :vant-icon="24" icon="home-o" style="font-size: 11px;" to="/index">首页</van-tabbar-item>
-              <van-tabbar-item icon="qr" style="font-size: 11px  ;height: 50px" to="/classify">分类</van-tabbar-item>
-              <van-tabbar-item icon="shop-o" style="font-size: 11px  ;height: 50px" to="/shop">店铺</van-tabbar-item>
-              <van-tabbar-item
-                icon="shopping-cart-o"
-                style="font-size: 11px  ;height: 50px"
-                to="/cart"
-                info="0"
-              >购物车</van-tabbar-item>
-              <van-tabbar-item icon="contact" style="font-size: 11px  ;height: 50px" to="/mine">我的</van-tabbar-item>
+              <van-tabbar-item icon="home-o" to="/index" size="24px">首页</van-tabbar-item>
+              <van-tabbar-item icon="qr" to="/classify">分类</van-tabbar-item>
+              <van-tabbar-item icon="shop-o" to="/shop">店铺</van-tabbar-item>
+              <van-tabbar-item icon="shopping-cart-o" to="/cart" info="0">购物车</van-tabbar-item>
+              <van-tabbar-item icon="contact" to="/mine">我的</van-tabbar-item>
             </van-tabbar>
           </footer>
         </van-col>
